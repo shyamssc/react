2cf06297c4660cae7e0c000053779e1a0079b175
Shyam Sundar Chaurasiya <44619884+shyamssc@users.noreply.github.com>
2020-04-15 18:17:11 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>news list </h1>
+<ul>
+<li> aaj tak </li>
+<li> abp news </li>
+<li> ndtc </li>
+ </ul>
+
+    </div>,
+    document.getElementById("root")
+);
+//It should display a h1 heading.
+//It should display an unordered list (bullet points).
+//It should contain 3 list elements.
